7f7a351cd72cd6a9eb3f805e9d8761574b4c04ed
Jakub Miłkowski <jakub.milkowski96@gmail.com>
2017-08-20 19:26:10 +0200

    [feat] Friend module mocks

--- src/modules/friends/components/friends.component.ts
+++ src/modules/friends/components/friends.component.ts
@@ -5,4 +5,39 @@ import {Component} from '@angular/core'
   templateUrl: './friends.component.html',
   styleUrls: ['./friends.component.css'],
 })
-export class FriendsComponent {}
+export class FriendsComponent {
+  friends = [
+    {
+      avatar: '/assets/avatars/1.jpg',
+      username: 'jaquelyn_4082'
+    },
+    {
+      avatar: '/assets/avatars/2.jpg',
+      username: 'cherryl_3611'
+    },
+    {
+      avatar: '/assets/avatars/3.jpg',
+      username: 'dotty1496'
+    },
+    {
+      avatar: '/assets/avatars/4.jpg',
+      username: 'lanora5515'
+    },
+    {
+      avatar: '/assets/avatars/5.jpg',
+      username: 'sharika8063'
+    },
+  ]
+
+  // People you may know
+  pumks = [
+    {
+      avatar: '/assets/avatars/6.jpg',
+      username: 'azucena-9656'
+    },
+    {
+      avatar: '/assets/avatars/7.jpg',
+      username: 'dionna9638'
+    },
+  ]
+}
--- src/modules/friends/friends.module.ts
+++ src/modules/friends/friends.module.ts
@@ -1,13 +1,20 @@
 import {NgModule} from '@angular/core'
 import {FriendsComponent} from './components/friends.component'
 import {FriendsRoutingModule} from './routing/friends-routing.module'
+import {MdButtonModule, MdIconModule, MdListModule} from '@angular/material'
+import {CommonModule} from '@angular/common'
 
 @NgModule({
   imports: [
+    CommonModule,
     FriendsRoutingModule,
+    MdIconModule,
+    MdListModule,
+    MdButtonModule,
   ],
   declarations: [
     FriendsComponent,
   ]
 })
-export class FriendsModule {}
+export class FriendsModule {
+}
