fa33f41fed0b23c7d7b8d7b07d048383ed7edf38
Jakub Miłkowski <jakub.milkowski96@gmail.com>
2017-08-25 23:32:52 +0200

    [feat] Update settings mock

--- src/modules/settings/components/settings/settings.component.ts
+++ src/modules/settings/components/settings/settings.component.ts
@@ -6,4 +6,50 @@ import {Component} from '@angular/core'
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent {
+  friends = [
+    {
+      username: 'jaquelyn_4082',
+      'avatar': '/assets/avatars/2.jpg',
+    },
+    {
+      username: 'cherryl_3611',
+      'avatar': '/assets/avatars/3.jpg',
+    },
+    {
+      username: 'dotty1496',
+      'avatar': '/assets/avatars/4.jpg',
+    },
+    {
+      username: 'lanora5515',
+      'avatar': '/assets/avatars/5.jpg',
+    },
+    {
+      username: 'sharika8063',
+      'avatar': '/assets/avatars/6.jpg',
+    },
+    {
+      username: 'azucena9656',
+      'avatar': '/assets/avatars/7.jpg',
+    },
+    {
+      username: 'dionna9638',
+      'avatar': '/assets/avatars/8.jpg',
+    },
+    {
+      username: 'setsuko9530',
+      'avatar': '/assets/avatars/9.jpg',
+    },
+    {
+      username: 'syreeta4792',
+      'avatar': '/assets/avatars/10.jpg',
+    },
+    {
+      username: 'syreeta4792',
+      'avatar': '/assets/avatars/11.jpg',
+    },
+    {
+      username: 'ozie_2912',
+      'avatar': '/assets/avatars/12.jpg',
+    },
+  ]
 }
--- src/modules/settings/settings.module.ts
+++ src/modules/settings/settings.module.ts
@@ -1,13 +1,25 @@
 import {NgModule} from '@angular/core'
 import {SettingsComponent} from './components/settings/settings.component'
 import {SettingsRoutingModule} from './routing/settings-routing.module'
-import {MdButtonModule, MdListModule} from '@angular/material'
+import {
+  MdButtonModule, MdIconModule, MdListModule, MdMenuModule,
+  MdProgressSpinnerModule,
+  MdTableModule
+} from '@angular/material'
+import {FriendsModule} from '../friends/friends.module'
+import {FriendsComponent} from '../friends/components/friends.component'
+import {CommonModule} from '@angular/common'
 
 @NgModule({
   imports: [
+    CommonModule,
     SettingsRoutingModule,
     MdButtonModule,
     MdListModule,
+    MdTableModule,
+    MdMenuModule,
+    MdIconModule,
+    MdProgressSpinnerModule,
   ],
   declarations: [
     SettingsComponent,
